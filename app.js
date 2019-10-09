8203970fcdd2b684d88dafb87a75c1e2c0226356
Tsawwassen <mitchell.rian.smith@gmail.com>
2019-10-08 20:59:43 -0700

    Add store_add function

--- app.js
+++ app.js
@@ -23,6 +23,15 @@ var db = mongojs('octagon', ['stores']);
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/loginapp');
 
+var formidable = require('formidable');
+
+// require bodyParser
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.get('/', function(req, res){
@@ -37,19 +46,39 @@ app.get('/store_add', function(req, res){
 
 app.post('/store_add', function(req, res){
 
-	db.stores.insert(req.body, function(err, docs){
-		if(err){
-			console.log(err);
+	db.stores.find({"storeNumber":req.body.storeNumber}, function(errFind, docsFind){
+		console.log(docsFind);
+		if(docsFind.length == 0){
+			db.stores.insert(req.body, function(err, docs){
+				if(err){
+					console.log(err);
+				}
+				else{
+					res.end("added");
+				}
+			});
+		} else {
+			res.end("!added");
 		}
-		else{
-			res.end("added");
-		}
+	
 	});
 
 });
 
+app.post('/stores_add', function(req, res){
+	var form = new formidable.IncomingForm();
+
+	console.log(req.data);
+
+	//form.parse(req, function (err, fields, files) {
+	//});
+	//console.log(form);
+	//console.log(req);
+
+});
+
 app.get('/store_view', function(req, res){
-	console.log("insdie get /store_view")
+	res.render('stores_view');
 });
 
 app.get('/store_edit', function(req, res){
